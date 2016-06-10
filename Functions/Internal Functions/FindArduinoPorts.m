4eb72a5fbb5672d4e3f1b28e4920fc1614bfb763
Josh Sanders <josh@sanworks.io>
2016-06-09 23:38:28 -0400

    -Added serial port auto-discovery for mac (contributed by Thiago Gouvea, Kepecs Lab, CSHL) -Deleted behavior data files

--- Functions/Internal Functions/FindArduinoPorts.m
+++ Functions/Internal Functions/FindArduinoPorts.m
@@ -10,8 +10,21 @@ if ispc
         ArduinoPorts{x} = Clip(1:find(Clip == 32,1, 'first')-1);
     end
     ArduinoPorts = ArduinoPorts(1:nPorts);
-elseif ismac
-    
+ elseif ismac % Contributed by Thiago Gouvea JUN_9_2016
+    [trash, RawSerialPortList] = system('ls /dev/tty.usbmodem*');
+    string = strtrim(RawSerialPortList);
+    PortStringPositions = strfind(string, '/dev/tty.usbmodem');
+    nPorts = length(PortStringPositions);
+    CandidatePorts = cell(1,nPorts);
+    nGoodPorts = 0;
+    for x = 1:nPorts
+        if PortStringPositions(x)+20 <= length(string)
+            CandidatePort = strtrim(string(PortStringPositions(x):PortStringPositions(x)+20));
+            nGoodPorts = nGoodPorts + 1;
+            CandidatePorts{nGoodPorts} = CandidatePort;
+        end
+    end
+    ArduinoPorts = CandidatePorts(1:nGoodPorts);
 else
     [trash, RawSerialPortList] = system('ls /dev/ttyACM*');
     string = strtrim(RawSerialPortList);

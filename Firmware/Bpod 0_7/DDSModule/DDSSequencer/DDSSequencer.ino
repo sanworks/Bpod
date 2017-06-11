6e744ac3c3b39360e2b59800542f2283757db1d0
Josh Sanders <josh@sanworks.io>
2017-06-11 16:10:55 -0400

    -Updated DDS module firmware to fix a bug with amplitude selection -Added DDSSequencer firmware, to play patterns of up to 10,000 discrete sine or triangle wave pulses on trigger.

--- Firmware/Bpod 0_7/DDSModule/DDSSequencer/DDSSequencer.ino
+++ Firmware/Bpod 0_7/DDSModule/DDSSequencer/DDSSequencer.ino
@@ -25,10 +25,7 @@
 // The module can output sine or triangle waves (menu function 'W') in frequencies between 1Hz and 100kHz.
 // Frequency (menu function 'F') can be controlled from: (0) USB, (1) the state machine or (2) a separate Bpod module, or any subset of these simultaneously.
 
-// This firmware supports streaming input control of frequency.
-// From separate module input (2), when setting frequency with 'F', a streaming value is specified in bits (16-bit resolution), 
-// and a configurable mapping function maps this value to compute frequency.
-// Inputs 0 and 1 set frequency with the 'F' menu function directly, by specifying it in Hz.
+// This firmware supports playback of pure tone sequences.
 
 #include "ArCOM.h"
 #include <SPI.h>
@@ -40,7 +37,7 @@ SPISettings DACSettings(4000000, MSBFIRST, SPI_MODE2); // Settings for DAC
 
 // Module setup
 uint32_t FirmwareVersion = 1;
-char moduleName[] = "DDSModule"; // Name of module for manual override UI and state machine assembler
+char moduleName[] = "DDSSeq"; // Name of module for manual override UI and state machine assembler
 
 // Pins
 const byte DACcs = 14; // DAC chip select
@@ -49,14 +46,24 @@ const byte DDScs = 15; // DDS chip select
 // DDS object
 AD983X_SW myDDS(DDScs, 10);
 
+// Timer object
+IntervalTimer hardwareTimer; // Hardware timer to ensure even sampling
+
+// Params
+uint32_t samplingRate = 10; // in Hz 
+uint32_t toneDuration = 1; // in samples
+uint32_t toneDurationMicroseconds = 100000; // in microseconds
+float frequency = 0; // Hz
+byte dataFormat = 0; // 0 = vector of frequencies 1 = alternating frequencies and onset times, using onset+toneDuration for offsets
+
 // Vars
 unsigned int dacVal = 0;
 union {
   byte Bytes[2];
-  int16_t Uint16[1];
+  uint16_t Uint16[1];
 } dacBuffer;
 byte dacBytes[2] = {0};
-float frequency = 0;
+
 unsigned long frequencyInt = 0;
 uint16_t adcValue = 0;
 uint32_t ampValue = 10000; // Value of amplitude scaling (0:10,000) 0 = 150mV, 10,000 = 650mV
@@ -64,9 +71,20 @@ byte op = 0;
 boolean newOp = false;
 byte opSource = 0;
 byte currentFrequencyRegister = 0;
-byte mappingFunctionIndex = 1; // 0 = linear, 1 = exponential
 byte currentWaveform = 0; // 0 = sine, 1 = triangle
-unsigned long frequencyRange[2] = {20, 17000}; // low end, high end in kHz, Preset to human auditory range
+const int maxSamples = 10000;
+double timerPeriod = 0;
+double idlePeriod = 100; // Period of timer while waiting for instructions
+uint32_t FreqPattern[maxSamples+2] = {0}; // Frequencies (+2 for look-ahead)
+uint16_t nSamples = 0;
+uint16_t iPattern = 0; // Index in the current pattern
+uint16_t currentSample = 0;
+uint32_t currentTime = 0;
+uint32_t nextToneOnsetTime = 0;
+uint32_t toneOffsetTime = 0;
+uint32_t patternEndTime = 0;
+uint32_t patternEndTimeMicroseconds = 0;
+boolean playingPattern = false;
 
 void setup() {
   Serial1.begin(1312500);
@@ -75,16 +93,22 @@ void setup() {
   SPI.beginTransaction(DACSettings);
   pinMode(DACcs, OUTPUT);
   digitalWrite(DACcs, HIGH);
-  frequency = 1000;
+  frequency = 0;
   myDDS.begin();
   delay(10);
   myDDS.setFrequency(0, frequency);
   myDDS.setOutputMode(OUTPUT_MODE_SINE); // OUTPUT_MODE_SINE OUTPUT_MODE_TRIANGLE
   dacVal = 0;
   dacWrite(dacVal);
+  timerPeriod = (1/(double)samplingRate)*1000000;
+  hardwareTimer.begin(handler, idlePeriod); // hardwareTimer is an interval timer object - Teensy 3.6's hardware timer
 }
 
 void loop() {
+  
+}
+
+void handler() {
   if (USBCOM.available() > 0) {
     op = USBCOM.readByte();
     newOp = true;
@@ -129,42 +153,18 @@ void loop() {
         myDDS.setFrequency(0, frequency);
         myDDS.setFrequency(1, frequency);
       break;
-      case 'M': // Map 16-bit value to frequency
-        switch (opSource) {
-          case 0:
-            adcValue = USBCOM.readUint16();
-            USBCOM.writeByte(1);
-          break;
-          case 1:
-            adcValue = StateMachineCOM.readUint16();
-          break;
-          case 2:
-            adcValue = ModuleCOM.readUint16();
-          break;
-        }
-        switch (mappingFunctionIndex) {
-          case 0:
-            frequency = linearMap(adcValue, 0, 65535, frequencyRange[0], frequencyRange[1]);
-          break;
-          case 1:
-            frequency = expMap(adcValue, 0, 65535, frequencyRange[0], frequencyRange[1]);
-          break;
-        }
-        myDDS.setFrequency(0, frequency);
-        myDDS.setFrequency(1, frequency);
-      break;
       case 'A': // Set amplitude. Range = 0 : 10000 ~ 150mV : 650mV p2p
-      switch (opSource) {
-          case 0:
-            ampValue = USBCOM.readUint32();
-            USBCOM.writeByte(1);
-          break;
-          case 1:
-            ampValue = StateMachineCOM.readUint32();
-          break;
-          case 2:
-            ampValue = ModuleCOM.readUint32();
-          break;
+        switch (opSource) {
+            case 0:
+              ampValue = USBCOM.readUint32();
+              USBCOM.writeByte(1);
+            break;
+            case 1:
+              ampValue = StateMachineCOM.readUint32();
+            break;
+            case 2:
+              ampValue = ModuleCOM.readUint32();
+            break;
         }
         dacVal = (uint16_t)(16383 - ((double)ampValue/10000)*16383);
         dacWrite(dacVal);
@@ -172,44 +172,56 @@ void loop() {
       case 'Q': // USB request to return current parameters
         USBCOM.writeUint32((uint32_t)(frequency*1000));
         USBCOM.writeUint32((uint32_t)(ampValue*1000));
+        USBCOM.writeUint32(samplingRate);
         USBCOM.writeByte(currentWaveform);
-        USBCOM.writeByte(mappingFunctionIndex);
-        USBCOM.writeUint32(frequencyRange[0]*1000);
-        USBCOM.writeUint32(frequencyRange[1]*1000);
+      break;
+      case 'D': // Set tone duration
+        toneDurationMicroseconds = USBCOM.readUint32();
+        USBCOM.writeByte(1); // Confirm byte
+        toneDuration = (uint32_t)((double)toneDurationMicroseconds/(double)timerPeriod);
       break;
       case 'V': // USB request to return frequency
         USBCOM.writeUint32((uint32_t)(frequency*1000));
       break;
-      case 'R': // Set frequency output range
-        switch (opSource) {
-          case 0:
-            frequencyRange[0] = USBCOM.readUint32();
-            frequencyRange[1] = USBCOM.readUint32();
-            USBCOM.writeByte(1);
-          break;
-          case 1:
-            frequencyRange[0] = StateMachineCOM.readUint32();
-            frequencyRange[1] = StateMachineCOM.readUint32();
-          break;
-          case 2:
-            frequencyRange[0] = ModuleCOM.readUint32();
-            frequencyRange[1] = ModuleCOM.readUint32();
-          break;
+      case 'T': // Set pattern offset time (loop mode)
+        patternEndTimeMicroseconds = USBCOM.readUint32();
+        patternEndTime = (uint32_t)((double)patternEndTimeMicroseconds/(double)timerPeriod);
+        USBCOM.writeByte(1); // Confirm byte
+      break;
+      case 'S': // Change sampling frequency
+          if (opSource == 0) {
+            samplingRate = USBCOM.readUint32();
+            hardwareTimer.end();
+            timerPeriod = (1/(double)samplingRate)*1000000;
+            hardwareTimer.begin(handler, timerPeriod);
+            USBCOM.writeByte(1); // Confirm byte
+            toneDuration = (uint32_t)((double)toneDurationMicroseconds/(double)timerPeriod);
+            patternEndTime = (uint32_t)((double)patternEndTimeMicroseconds/(double)timerPeriod);
+          }
+      break;
+      case 'L': // Load pattern
+        if (opSource == 0) {
+          dataFormat = USBCOM.readByte();
+          nSamples = USBCOM.readUint16();
+          USBCOM.readUint32Array(FreqPattern, nSamples+(nSamples*dataFormat));
+          USBCOM.writeByte(1);
         }
       break;
-      case 'N': // Set mapping function by index
-        switch (opSource) {
-          case 0:
-            mappingFunctionIndex = USBCOM.readByte();
-            USBCOM.writeByte(1);
-          break;
-          case 1:
-            mappingFunctionIndex = StateMachineCOM.readByte();
-          break;
-          case 2:
-            mappingFunctionIndex = ModuleCOM.readByte();
-          break;
+      case 'P': // Play pattern
+        playingPattern = true;
+        currentTime = 0;
+        currentSample = 0;
+        iPattern = 0;
+        nextToneOnsetTime = 0;
+        if (dataFormat > 0) {
+          nextToneOnsetTime = FreqPattern[1];
         }
+        hardwareTimer.end();
+        hardwareTimer.begin(handler, timerPeriod); // hardwareTimer is an interval timer object - Teensy 3.6's hardware timer
+      break;
+      case 'X': // Stop pattern
+        freezeDDS();
+        stopPlayback();
       break;
       case 'W': // Set waveform by index
         switch (opSource) {
@@ -235,6 +247,65 @@ void loop() {
       break;
     }
   }
+  if (playingPattern) {
+    if (currentSample == nSamples) {
+      if (patternEndTime > 0) {
+        iPattern = 0;
+        currentSample = 0;
+      }
+    }
+    if (currentSample < nSamples) {
+      if ((dataFormat == 0) || (currentTime == nextToneOnsetTime)) {
+        frequency = ((double)FreqPattern[iPattern])/1000;
+        myDDS.setFrequency(0, frequency);
+        myDDS.setFrequency(1, frequency);
+        if (dataFormat > 0) { 
+          iPattern++;
+          nextToneOnsetTime = FreqPattern[iPattern+2];
+          toneOffsetTime = currentTime + toneDuration;
+        }
+        iPattern++;
+        currentSample++;
+      }
+    } else {
+      if ((dataFormat == 0) && (patternEndTime == 0)) {
+          freezeDDS();
+          stopPlayback();
+      }
+    }
+    if (dataFormat > 0) {
+      if (currentTime == toneOffsetTime) {
+        freezeDDS();
+        if (currentSample == nSamples) {
+          if (patternEndTime == 0) {
+            stopPlayback();
+          } else {
+            iPattern = 0;
+            currentSample = 0;
+          }
+        }
+      }
+    }
+    currentTime++;
+    if ((currentTime >= patternEndTime) && (patternEndTime > 0)) {
+      freezeDDS();
+      playingPattern = false;
+      hardwareTimer.end();
+      hardwareTimer.begin(handler, idlePeriod);
+    }
+  }
+}
+
+void freezeDDS() {
+  frequency = 0;
+  myDDS.setFrequency(0, frequency);
+  myDDS.setFrequency(1, frequency);
+}
+
+void stopPlayback() {
+  playingPattern = false;
+  hardwareTimer.end();
+  hardwareTimer.begin(handler, idlePeriod);
 }
 
 void dacWrite(uint16_t value) {
@@ -245,15 +316,6 @@ void dacWrite(uint16_t value) {
   digitalWrite(DACcs, HIGH);
 }
 
-double linearMap(uint16_t x, uint32_t in_min, uint32_t in_max, uint32_t out_min, uint32_t out_max) {
-  return ((double)x - (double)in_min) * ((double)out_max - (double)out_min) / ((double)in_max - (double)in_min) + (double)out_min;
-}
-
-double expMap(uint16_t input, uint32_t in_min, uint32_t in_max, uint32_t out_min, uint32_t out_max) {
-  uint32_t inputRange = in_max - in_min;
-  uint32_t outputRange = out_max - out_min;
-  return ((sq((double)input-(double)in_min)/sq((double)inputRange))*(double)outputRange)+(double)out_min;
-}
 void returnModuleInfo() { // Return module name and firmware version
   StateMachineCOM.writeByte(65); // Acknowledge
   StateMachineCOM.writeUint32(FirmwareVersion); // 4-byte firmware version
